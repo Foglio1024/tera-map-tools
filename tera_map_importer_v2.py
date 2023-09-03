b86f488f6dfc6737e0f81f18ad5b879c54475f5e
Foglio <foglio1024@gmail.com>
2023-09-03 14:39:16 +0200

    big refactoring

--- tera_map_importer_v2.py
+++ tera_map_importer_v2.py
@@ -4,61 +4,67 @@ import math
 from mathutils import Vector
 from bpy_extras.object_utils import object_data_add
 
+class Utils:
+    def parse_name(line):
+        return line[line.find("Name=") + 5 : -1].replace('\"', '')
 
-class Printer:
-    def __init__(self):
-        self.text = ""
+    def parse_class(line):
+        class_idx = line.find('Class=')
+        class_name = ''
+        if class_idx != -1:
+            class_name = line[class_idx:line.find('Name=')].replace('Class=/Script/Engine.', '').trim()
 
-    def reprint(self, text):
-        empty = ""
-        for c in self.text:
-            empty += " "
-        print(empty, end="\r")
-        print(text, end="\r")
-        self.text = text
+        return class_name
 
-    def print(self, text):
-        self.reprint(text + "\n")
+    def parse_vector(line, name):
+        split = line.replace(f'{name}=', '').replace('(', '').replace(')', '').split(',')
 
-printer = Printer()
+        ret = [] # x, y, z
 
-# -------------------------- set paths -----------------------------
+        for part in split:
+            value = float(part.split('=')[1])
+            ret.append(value)
+        
+        return ret
 
-SRC_DIR = 'E:\\TERA_DEV\\test_re_export\\cli'
-
-MAP_NAME = 'RNW_C_P'
-
-LEVEL_DIR = f'{SRC_DIR}\\{MAP_NAME}'
-T3D_PATH = f'{LEVEL_DIR}\\{MAP_NAME}.t3d'
-TERRAIN_PATH = f'{LEVEL_DIR}\\Terrains.txt'
-
-# -------------------------------------------------------------------
-
-def parse_name(line):
-    return line[line.find("Name=") + 5 : -1].replace('\"', '')
-
-def parse_class(line):
-    class_idx = line.find('Class=')
-    class_name = ''
-    if class_idx != -1:
-        class_name = line[class_idx:line.find('Name=')].replace('Class=/Script/Engine.', '').trim()
-
-    return class_name
-
-def parse_vector(line, name):
-    split = line.replace(f'{name}=(', '').replace(')', '').split(',')
-
-    ret = [] # x, y, z
-
-    for part in split:
-        value = float(part.split('=')[1])
-        ret.append(value)
+    def parse_mesh_path(line):
+        ret =  line[line.find('\"'):].replace('\"', '').replace('\'', '') + ".psk"
+        return ret.replace('/Game', 'StaticMeshes\\')
     
-    return ret
+    def find_or_create_collection(coll_name):
+        found = None
+        for c in bpy.data.collections:
+            if c.name == coll_name:
+                found = c
+        if found == None:
+            found = bpy.data.collections.new(coll_name)
+            bpy.context.scene.collection.children.link(found)
 
-def parse_mesh_path(line):
-    ret =  line[line.find('\"'):].replace('\"', '').replace('\'', '') + ".psk"
-    return ret.replace('/Game', 'StaticMeshes\\')
+        return found
+
+    def reframe(collection):
+
+        pivot = bpy.data.objects.new("Pivot", None)
+
+        for obj in collection.objects:
+            obj.parent = pivot
+        
+        collection.objects.link(pivot)
+
+        pivot.scale.x *= 0.01 * (-1)
+        pivot.scale.y *= 0.01
+        pivot.scale.z *= 0.01
+
+        pivot.rotation_euler[2] = math.radians(-90)
+
+        bpy.context.evaluated_depsgraph_get().update()
+        for child in pivot.children:
+            pwm = child.matrix_world.copy()
+            child.parent = None
+            child.matrix_world = pwm
+
+        bpy.data.objects.remove(bpy.data.objects[pivot.name])
+
 
 class Level:
     def __init__(self, name):
@@ -67,7 +73,7 @@ class Level:
 
     def read_from(file_path):
 
-        printer.print(f'Loading level from {file_path}')
+        print(f'Loading level from {file_path}')
 
         h,t = os.path.split(file_path)
 
@@ -79,22 +85,22 @@ class Level:
         actor_labels = []
 
         lines = open(file_path, "r").readlines()
-        printer.print(f'Read {len(lines)} lines')
+        print(f'Read {len(lines)} lines')
 
         idx = 0
         for line in lines:
             line = line.strip()
             if line.startswith("Begin Actor"):
-                actor_name = parse_name(line)
-                current_actor = Actor(f'{actor_name}_{idx}', idx)
+                actor_name = Utils.parse_name(line)
+                current_actor = StaticMeshActor(f'{actor_name}_{idx}', idx)
                 idx = idx + 1
                 actor_labels.append(actor_name)
-                printer.reprint(f'Created Actor {actor_name}')
+                print(f'Created Actor {actor_name}')
             elif line.startswith('ActorLabel'):
                 current_actor.set_label(line)
 
             elif line.startswith("Begin Object"):
-                obj_name = parse_name(line)
+                obj_name = Utils.parse_name(line)
                 smc = StaticMeshComponent(obj_name)
             elif line.startswith("End Object"):
                 if smc.mesh_path == '': continue
@@ -109,7 +115,7 @@ class Level:
                 smc.read_line(line)
         return ret
 
-class Actor:
+class StaticMeshActor:
     def __init__(self, name, idx):
         self.name = name
         self.smc = None
@@ -129,14 +135,28 @@ class StaticMeshComponent:
     
     def read_line(self, line):
         if line.find('RelativeLocation') != -1:
-            self.location = parse_vector(line, 'RelativeLocation')
+            self.location = Utils.parse_vector(line, 'RelativeLocation')
         elif line.find('RelativeRotation') != -1:
-            self.rotation = parse_vector(line, 'RelativeRotation')
+            self.rotation = Utils.parse_vector(line, 'RelativeRotation')
         elif line.find('RelativeScale3D') != -1:
-            self.scale = parse_vector(line, 'RelativeScale3D')
+            self.scale = Utils.parse_vector(line, 'RelativeScale3D')
         elif line.find('StaticMesh=StaticMesh') != -1:
-            self.mesh_path = parse_mesh_path(line)
-            printer.reprint(f'Set mesh path: {self.mesh_path}')
+            self.mesh_path = Utils.parse_mesh_path(line)
+            print(f'Set mesh path: {self.mesh_path}')
+    
+        
+    def apply_transform_to(self, obj):
+        obj.scale.x = self.scale[0]
+        obj.scale.y = self.scale[1] * (-1)
+        obj.scale.z = self.scale[2]
+
+        obj.rotation_euler.x = math.radians(self.rotation[2] * (-1))
+        obj.rotation_euler.y = math.radians(self.rotation[0])
+        obj.rotation_euler.z = math.radians(self.rotation[1])
+
+        obj.location.x = self.location[0]
+        obj.location.y = self.location[1]
+        obj.location.z = self.location[2]
 
 class Terrain:
     def read_from(file_path):
@@ -145,7 +165,7 @@ class Terrain:
 
         if not os.path.exists(file_path): return ret
 
-        printer.print(f'Reading terrains from {file_path}')
+        print(f'Reading terrains from {file_path}')
 
         current_terrain = None
         lines = open(file_path, "r").readlines()
@@ -154,9 +174,9 @@ class Terrain:
             if line.startswith('\t'):
                 line = line.strip().replace(': ', '=')
                 if 'Location' in line:
-                    current_terrain.abs_location = parse_vector(line, 'Location')
+                    current_terrain.abs_location = Utils.parse_vector(line, 'Location')
                 elif 'Scale' in line:
-                    current_terrain.abs_scale = parse_vector(line, 'Scale')
+                    current_terrain.abs_scale = Utils.parse_vector(line, 'Scale')
             else:
                 if current_terrain != None:
                     ret.append(current_terrain)
@@ -173,9 +193,9 @@ class Terrain:
             setup_lines = open(setup_path, "r").readlines()
             for line in setup_lines:
                 if line.startswith('Location'):
-                        terrain.rel_location = parse_vector(line, 'Location')
+                        terrain.rel_location = Utils.parse_vector(line, 'Location')
                 if line.startswith('Scale'):
-                        terrain.rel_scale = parse_vector(line, 'Scale')
+                        terrain.rel_scale = Utils.parse_vector(line, 'Scale')
 
         return ret
     
@@ -184,164 +204,123 @@ class Terrain:
         self.map = line[:idx]
         self.name = line[idx + 1:]
 
-# -------------------------------------------------------------------
+class MapImporter:
 
-def find_or_create_collection(coll_name):
-    found = None
-    for c in bpy.data.collections:
-        if c.name == coll_name:
-            found = c
-    if found == None:
-        found = bpy.data.collections.new(coll_name)
-        bpy.context.scene.collection.children.link(found)
+    def __init__(self, source_dir, map_name):
+        self.source_dir = source_dir
+        self.map_name = map_name
+        self.level_dir = f'{source_dir}\\{map_name}'
+        self.t3d_path = f'{self.level_dir}\\{map_name}.t3d'
+        self.terrains_path = f'{self.level_dir}\\Terrains.txt'
 
-    return found
 
-def set_transform(smc, obj):
-    obj.scale.x = smc.scale[0]
-    obj.scale.y = smc.scale[1] * (-1)
-    obj.scale.z = smc.scale[2]
+    def import_actors(self, level):
+        loaded_meshes = []
+        idx = 0
 
-    obj.rotation_euler.x = math.radians(smc.rotation[2] * (-1))
-    obj.rotation_euler.y = math.radians(smc.rotation[0])
-    obj.rotation_euler.z = math.radians(smc.rotation[1])
+        for sma in level.actors:
+            idx += 1
+            mesh_path = os.path.join(self.source_dir, level.name, sma.smc.mesh_path)
 
-    obj.location.x = smc.location[0]
-    obj.location.y = smc.location[1]
-    obj.location.z = smc.location[2]
+            if not os.path.exists(mesh_path):
+                print(f"Cannot find mesh file in {mesh_path}")
+            else:
+                imported = None
+                for loaded_name, loaded_path in loaded_meshes:
+                    if loaded_path == mesh_path:
+                        print(f"Copying {sma.label} <- {loaded_name}")
+                        src = bpy.data.objects[loaded_name]
+                        cp = src.copy()
+                        cp.data = src.data
+                        imported = cp
+                        break
 
-def import_actors(level, map_coll, pivot):
-    loaded_meshes = []
-    idx = 0
+                if imported == None:
+                    print(f"Importing {mesh_path}")
+                    bpy.ops.import_scene.psk(filepath=mesh_path)
+                    imported = bpy.context.scene.collection.objects[0]
+                    loaded_meshes.append(
+                        (f"{sma.label}_{sma.index}", mesh_path)
+                    )
 
-    for sma in level.actors:
-        idx += 1
-        mesh_path = SRC_DIR + '\\' +level.name + '\\' + sma.smc.mesh_path #os.path.join(SRC_DIR, level.name, sma.smc.mesh_path)
+                mesh_name = imported.data.name
+                imported.name = f"{sma.label}_{sma.index}"
+                imported.data.name = mesh_name
+                sma.smc.apply_transform_to(imported)
 
-        if not os.path.exists(mesh_path):
-            printer.print(
-                f"Cannot find mesh file in {mesh_path}"
-            )
-        else:
-            imported = None
-            for loaded_name, loaded_path in loaded_meshes:
-                if loaded_path == mesh_path:
-                    printer.reprint(f"Copying {sma.label} <- {loaded_name}")
-                    src = bpy.data.objects[loaded_name]
-                    cp = src.copy()
-                    cp.data = src.data
-                    imported = cp
-                    break
+                self.map_coll.objects.link(imported)
+                # unlink object from main collection
+                if imported.name in bpy.context.scene.collection.objects:
+                    bpy.context.scene.collection.objects.unlink(imported)
 
-            if imported == None:
-                printer.reprint(f"Importing {mesh_path}")
-                bpy.ops.import_scene.psk(filepath=mesh_path)
-                imported = bpy.context.scene.collection.objects[0]
-                loaded_meshes.append(
-                    (f"{sma.label}_{sma.index}", mesh_path)
-                )
+    def import_terrains(self, terrains):
+        for ter in terrains:
+            # create square
+            terrain_mesh = bpy.data.meshes.new(f'{ter.map}_{ter.name}')
+            points = []
+            points.append(Vector([ 0    , 0    , 0 ]))
+            points.append(Vector([-15360, 0    , 0 ]))
+            points.append(Vector([-15360, 15360, 0 ]))
+            points.append(Vector([ 0    , 15360, 0 ]))
 
-            mesh_name = imported.data.name
-            imported.name = f"{sma.label}_{sma.index}"
-            imported.data.name = mesh_name
-            set_transform(sma.smc, imported)
+            terrain_mesh.from_pydata(points, [[0, 1], [1, 2], [2, 3], [3, 0]], [[1,2,3,0]])
+            terrain_mesh.uv_layers.new(name='HeightUV')
+            obj = bpy.data.objects.new(terrain_mesh.name, terrain_mesh)
 
-            imported.parent = pivot
+            # add subsurf mod
+            subsurf = obj.modifiers.new("Subdivision", 'SUBSURF')
+            subsurf.levels = 9 # todo: check
+            subsurf.subdivision_type = 'SIMPLE'
+            # add displacement mod
+            displace = obj.modifiers.new("Displace", 'DISPLACE')
+            # configure displacement mod
+            displace.strength = 1310.5 # todo: why?
+            displace.mid_level = 0.5
 
-            map_coll.objects.link(imported)
+            # create texture for displacement
+            tex = bpy.data.textures.new('HeightMap', 'IMAGE')
+            displace.texture = tex
+            displace.texture_coords = 'UV'
+
+            # load image from height map file
+            img = bpy.data.images.load(os.path.join(self.level_dir, 'Terrains', ter.map, ter.name, 'HeightMap.png'))
+            img.colorspace_settings.name = 'Non-Color'
+            tex.image = img
+            tex.extension = 'EXTEND'
+            tex.use_interpolation = False
+
+            # move square to raw position
+            obj.location.x = ter.rel_location[0]
+            obj.location.y = ter.rel_location[1]
+            obj.location.z = ter.rel_location[2]
+
+            obj.scale.x = 4
+            obj.scale.y = 4
+            obj.scale.z = -100
+
+            obj.rotation_euler.z = math.radians(-90)
+
+            # link to collection
+            self.map_coll.objects.link(obj)
+
             # unlink object from main collection
-            if imported.name in bpy.context.scene.collection.objects:
-                bpy.context.scene.collection.objects.unlink(imported)
+            if obj.name in bpy.context.scene.collection.objects:
+                bpy.context.scene.collection.objects.unlink(obj)
 
-def import_terrains(terrains, map_coll, pivot):
-    for ter in terrains:
-        # create square
-        terrain_mesh = bpy.data.meshes.new(f'{ter.map}_{ter.name}')
-        points = []
-        points.append(Vector([ 0    , 0    , 0 ]))
-        points.append(Vector([-15360, 0    , 0 ]))
-        points.append(Vector([-15360, 15360, 0 ]))
-        points.append(Vector([ 0    , 15360, 0 ]))
 
-        terrain_mesh.from_pydata(points, [[0, 1], [1, 2], [2, 3], [3, 0]], [[1,2,3,0]])
-        terrain_mesh.uv_layers.new(name='HeightUV')
-        obj = bpy.data.objects.new(terrain_mesh.name, terrain_mesh)
+    def import_level(self):
 
-        # add subsurf mod
-        subsurf = obj.modifiers.new("Subdivision", 'SUBSURF')
-        subsurf.levels = 9 # todo: check
-        subsurf.subdivision_type = 'SIMPLE'
-        # add displacement mod
-        displace = obj.modifiers.new("Displace", 'DISPLACE')
-        # configure displacement mod
-        displace.strength = 1310.5 # todo: why?
-        displace.mid_level = 0.5
+        level = Level.read_from(self.t3d_path)
+        terrains = Terrain.read_from(self.terrains_path)
 
-        # create texture for displacement
-        tex = bpy.data.textures.new('HeightMap', 'IMAGE')
-        displace.texture = tex
-        displace.texture_coords = 'UV'
+        self.map_coll = Utils.find_or_create_collection(level.name)
 
-        # load image from height map file
-        img = bpy.data.images.load(os.path.join(LEVEL_DIR, 'Terrains', ter.map, ter.name, 'HeightMap.png'))
-        img.colorspace_settings.name = 'Non-Color'
-        tex.image = img
-        tex.extension = 'EXTEND'
-        tex.use_interpolation = False
+        self.import_actors(level)
+        self.import_terrains(terrains)
 
-        # move square to raw position
-        obj.location.x = ter.rel_location[0]
-        obj.location.y = ter.rel_location[1]
-        obj.location.z = ter.rel_location[2]
-
-        obj.scale.x = 4
-        obj.scale.y = 4
-        obj.scale.z = -100
-
-        obj.rotation_euler.z = math.radians(-90)
-
-        # link to collection
-        map_coll.objects.link(obj)
-
-        # unlink object from main collection
-        if obj.name in bpy.context.scene.collection.objects:
-            bpy.context.scene.collection.objects.unlink(obj)
-
-        # parent to pivot
-        obj.parent = pivot
-
-def reframe(pivot):
-    pivot.scale.x *= 0.01 * (-1)
-    pivot.scale.y *= 0.01
-    pivot.scale.z *= 0.01
-
-    pivot.rotation_euler[2] = math.radians(-90)
-
-    bpy.context.evaluated_depsgraph_get().update()
-    for child in pivot.children:
-        pwm = child.matrix_world.copy()
-        child.parent = None
-        child.matrix_world = pwm
-
-    bpy.data.objects.remove(bpy.data.objects[pivot.name])
-
-def import_level(level, terrains):
-
-    map_coll = find_or_create_collection(level.name)
-
-    pivot = bpy.data.objects.new("Pivot", None)
-    map_coll.objects.link(pivot)
-
-    import_actors(level, map_coll, pivot)
-
-    import_terrains(terrains, map_coll, pivot)
-
-    reframe(pivot)
-
+        Utils.reframe(self.map_coll)
 
 # -------------------------------------------------------------------
 
-level = Level.read_from(T3D_PATH)
-terrains = Terrain.read_from(TERRAIN_PATH)
-
-import_level(level, terrains)
+importer = MapImporter(source_dir='E:\\TERA_DEV\\test_re_export\\cli', map_name='RNW_C_P')
+importer.import_level()

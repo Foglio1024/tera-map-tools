50d4047a539d9720291d04a87f956238a32cb6e8
Foglio <foglio1024@gmail.com>
2023-09-05 15:21:32 +0200

    implement working volumes generation algorithm

--- ray_cast.py
+++ ray_cast.py
@@ -1,8 +1,25 @@
-import bpy
+import time
 import math
+import bpy
 from mathutils import Vector
 from bpy_extras.object_utils import object_data_add
 
+class Printer:
+    def __init__(self):
+        self.text = ''
+
+    def reprint(self, text):
+        empty = ''
+        for c in self.text:
+            empty += ' '
+        print(empty, end='\r')
+        print(text, end='\r')
+        self.text = text
+
+    def print(self, text):
+        self.reprint(text + '\n')
+
+printer = Printer()
 
 def raycast(x, y):
     result, location, normal, index, object, matrix = bpy.context.scene.ray_cast(
@@ -11,7 +28,7 @@ def raycast(x, y):
         Vector((0, 0, -1)),
         distance=40000,
     )
-    return location.z
+    return result, location.z, object
 
 
 SCENE_SCALE = bpy.context.scene.unit_settings.scale_length
@@ -20,50 +37,99 @@ ZONE_SIZE = 15360
 
 SQUARE_SIZE = ZONE_SIZE / 120
 
-ZONE_OFFSET = [0, 1]
+ZONE_OFFSET = Vector([0, 1, 0])
+
+SRC_COLLECTION = 'RNW_C_P'
+done_coll = bpy.data.collections.new(SRC_COLLECTION + '_hit')
+geo_coll = bpy.data.collections.new(SRC_COLLECTION + '_geo')
+bpy.context.scene.collection.children.link(done_coll)
+bpy.context.scene.collection.children.link(geo_coll)
+bpy.context.window.view_layer.layer_collection.children[geo_coll.name].exclude = True
+bpy.context.window.view_layer.layer_collection.children[done_coll.name].exclude = True
+
+objects_hit = []
+
+volume_idx = 0
+
+num_squares = 120
+num_cells = 8
+
+# missed_points = []
+missed_points = {}
+start = time.time()
+while True:
+
+    volume_start = time.time()
+    volume_squares = []
+    volume_name = f'Volume_{volume_idx}'
+    volume_mesh = bpy.data.meshes.new(volume_name)
+    volume_obj = bpy.data.objects.new(volume_name, volume_mesh)
+    volume_points = []
+    volume_data = []
+
+    for sx in range(num_squares):
+        for sy in range(num_squares):
+            # printer.reprint(f'Square: {x},{y} @ {volume_idx}')
+            square_location_x = (
+                sx * (ZONE_SIZE / 120) + ZONE_OFFSET.x * ZONE_SIZE
+            ) / SCENE_SCALE
+            square_location_y = (
+                sy * (ZONE_SIZE / 120) + ZONE_OFFSET.y * ZONE_SIZE
+            ) / SCENE_SCALE
+
+            cell_times = []
+
+            for cx in range(num_cells):
+                for cy in range(num_cells):
+                    cell_x = (cx + 0.5) * ((128/num_cells) / SCENE_SCALE)
+                    cell_y = (cy + 0.5) * ((128/num_cells) / SCENE_SCALE)
+
+                    abs_x = cell_x + square_location_x
+                    abs_y = cell_y + square_location_y
+
+                    cell_idx = cx + (num_cells * cy) + (sx + (num_squares * sy)) * num_cells * num_cells
+
+                    # point_key = f'{abs_x}_{abs_y}'
+
+                    if cell_idx in missed_points: continue
+                    cell_start = time.time()
+                    result, z, obj = raycast(abs_x, abs_y)
+                    if result:
+                        volume_points.append([abs_x, abs_y, z])
+                        if obj.name not in objects_hit:
+                            objects_hit.append(obj.name)
+                    else:
+                        # missed_points.append([abs_x, abs_y])
+                        missed_points[cell_idx] = True
+                    cell_end = time.time()
+                    cell_times.append(cell_end - cell_start)
+
+            avg = 0
+            if len(cell_times) != 0:
+                avg = sum(cell_times)/len(cell_times)
+            printer.reprint(f'Square: {sx},{sy} @ {volume_idx} ({avg}s ray cast average)')
 
 
-for x in range(120):
-    for y in range(120):
-        print(f'Square: {x},{y}')
-        rect_mesh = bpy.data.meshes.new(f"S_x{x}y{y}")
-        rect = bpy.data.objects.new(f"S_x{x}y{y}", rect_mesh)
-        rect.location.x = (
-            x * (ZONE_SIZE / 120) + ZONE_OFFSET[0] * ZONE_SIZE
-        ) / SCENE_SCALE
-        rect.location.y = (
-            y * (ZONE_SIZE / 120) + ZONE_OFFSET[1] * ZONE_SIZE
-        ) / SCENE_SCALE
+    if len(volume_points) != 0:
+        volume_mesh.from_pydata(volume_points, [], [])
+        geo_coll.objects.link(volume_obj)
+        volume_squares.append(volume_obj)
 
-        square_points = [
-            # [0, 0, 0],
-            # [0, SQUARE_SIZE / SCENE_SCALE, 0],
-            # [SQUARE_SIZE / SCENE_SCALE, SQUARE_SIZE / SCENE_SCALE, 0],
-            # [SQUARE_SIZE / SCENE_SCALE, 0, 0],
-        ]
-        square_edges = []  # [[0,1],[1,2],[2,3],[3,0]]
-        square_faces = []  # [[0,1,2,3,0]]
+    objects_hit_count = len(objects_hit)
 
-        for cx in range(8):
-            for cy in range(8):
-                cell_x = (cx + 0.5) * (16 / SCENE_SCALE)
-                cell_y = (cy + 0.5) * (16 / SCENE_SCALE)
-                z = raycast(cell_x + rect.location.x, cell_y + rect.location.y)
-                square_points.append([cell_x, cell_y, round(z, 1)])
+    if len(objects_hit) != 0:
+        for hit_object_name in objects_hit:
+            done_coll.objects.link(bpy.data.objects[hit_object_name])
+            bpy.data.collections[SRC_COLLECTION].objects.unlink(bpy.data.objects[hit_object_name])
 
-        rect_mesh.from_pydata(square_points, square_edges, square_faces)
-        bpy.context.scene.collection.objects.link(rect)
+        objects_hit.clear()
+
+        volume_end = time.time()
+        printer.print(f'Volume {volume_idx} done in {volume_end - volume_start}s - removed {objects_hit_count} objects')
+        volume_idx+=1
+    else: break
 
 
-# result, location, normal, index, object, matrix = bpy.context.scene.ray_cast(bpy.context.view_layer.depsgraph, bpy.data.objects['Empty'].location , Vector((0,0,-1)), distance=10000)
+end = time.time()
 
-# loc_obj = bpy.data.objects.new('loc', None)
-# loc_obj.location = location
-# bpy.context.scene.collection.objects.link(loc_obj)
-
-# print(result)
-# print(location)
-# print(normal)
-# print(index)
-# print(object)
-# print(matrix)
+printer.print(f'Total time: {end - start}')
